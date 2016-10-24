229bc3f39a04aba828663acb96d9cf97fed7d75c
Casey Kulm <mlukyesac@gmail.com>
2016-10-23 21:43:52 -0500

    Update RxJava example

--- samples/src/main/java/com/example/retroravelry/patterns/RxJavaGetPatterns.java
+++ samples/src/main/java/com/example/retroravelry/patterns/RxJavaGetPatterns.java
@@ -4,22 +4,28 @@ import com.caseykulm.retroravelry.RxRetroRavelryService;
 import com.caseykulm.retroravelry.responses.patterns.SearchPatternsResponse;
 import com.example.retroravelry.ServiceFactory;
 import rx.Single;
+import rx.Subscription;
 import rx.functions.Action1;
 
 public class RxJavaGetPatterns extends BaseGetPatterns {
 
+  private Action1<SearchPatternsResponse> success = new Action1<SearchPatternsResponse>() {
+    public void call(SearchPatternsResponse response) {
+      printPatterns(response);
+    }
+  };
+
+  private Action1<Throwable> failure = new Action1<Throwable>() {
+    public void call(Throwable throwable) {
+      printErrors(throwable);
+    }
+  };
+
   @Override void getPatterns() {
     RxRetroRavelryService service = ServiceFactory.newRxService();
     Single<SearchPatternsResponse> call = service.searchPatterns("", 0, 10, false);
-    call.subscribe(new Action1<SearchPatternsResponse>() {
-      public void call(SearchPatternsResponse response) {
-        printPatterns(response);
-      }
-    }, new Action1<Throwable>() {
-      public void call(Throwable throwable) {
-        printErrors(throwable);
-      }
-    });
+    Subscription subscription = call.subscribe(success, failure);
+    // need to kill this subscription when done
   }
 
 }
