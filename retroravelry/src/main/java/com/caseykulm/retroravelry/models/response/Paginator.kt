0c02f7b947f8ba7598ca2ad9a264c506cde84230
Casey Kulm <mlukyesac@gmail.com>
2019-07-21 08:53:07 -0600

    Bump versions for stuff

--- retroravelry/src/main/java/com/caseykulm/retroravelry/models/response/Paginator.kt
+++ retroravelry/src/main/java/com/caseykulm/retroravelry/models/response/Paginator.kt
@@ -1,11 +1,9 @@
 package com.caseykulm.retroravelry.models.response
 
-import com.squareup.moshi.Json
-
 data class Paginator (
-        val last_page: Int,
-        val page: Int,
-        val page_count: Int,
-        val page_size: Int,
-        val results: Int
+    val last_page: Int,
+    val page: Int,
+    val page_count: Int,
+    val page_size: Int,
+    val results: Int
 )
