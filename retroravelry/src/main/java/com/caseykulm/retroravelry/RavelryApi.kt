c261caa49261223519a43387615368a03cbf982c
Casey Kulm <mlukyesac@gmail.com>
2019-08-17 12:51:59 -0600

    Convert API to RxJava Single only

--- retroravelry/src/main/java/com/caseykulm/retroravelry/RavelryApi.kt
+++ retroravelry/src/main/java/com/caseykulm/retroravelry/RavelryApi.kt
@@ -6,20 +6,13 @@ import com.caseykulm.retroravelry.network.responses.library.LibraryResponse
 import com.caseykulm.retroravelry.network.responses.patterns.SearchPatternsResponse
 import com.caseykulm.retroravelry.network.responses.patterns.ShowPatternResponse
 import com.caseykulm.retroravelry.network.responses.photos.ShowPhotoSizesResponse
-import io.reactivex.Flowable
-import retrofit2.Call
+import io.reactivex.Single
 import retrofit2.adapter.rxjava2.Result
 
 interface RavelryApi {
-  fun searchPatternsRx(
-      query: String, page: Int, pageSize: Int): Flowable<Result<SearchPatternsResponse>>
+  fun searchPatternsRx(query: String, page: Int, pageSize: Int): Single<Result<SearchPatternsResponse>>
 
-  fun searchPatterns(
-      query: String, page: Int, pageSize: Int): Call<SearchPatternsResponse>
-
-  fun showPatternRx(id: Int): Flowable<Result<ShowPatternResponse>>
-
-  fun showPattern(id: Int): Call<ShowPatternResponse>
+  fun showPatternRx(id: Int): Single<Result<ShowPatternResponse>>
 
   fun searchMyLibraryRx(
       query: String,
@@ -27,15 +20,8 @@ interface RavelryApi {
       type: Type?,
       sort: Sort?,
       page: Int,
-      pageSize: Int): Flowable<Result<LibraryResponse>>
-
-  fun searchMyLibrary(
-      query: String,
-      queryType: String?,
-      type: Type?,
-      sort: Sort?,
-      page: Int,
-      pageSize: Int): Call<LibraryResponse>
+      pageSize: Int
+  ): Single<Result<LibraryResponse>>
 
   fun searchLibraryRx(
       username: String,
@@ -44,18 +30,8 @@ interface RavelryApi {
       type: Type?,
       sort: Sort?,
       page: Int,
-      pageSize: Int): Flowable<Result<LibraryResponse>>
+      pageSize: Int
+  ): Single<Result<LibraryResponse>>
 
-  fun searchLibrary(
-      username: String,
-      query: String,
-      queryType: String?,
-      type: Type?,
-      sort: Sort?,
-      page: Int,
-      pageSize: Int): Call<LibraryResponse>
-
-  fun showPhotoSizesRx(photoId: String): Flowable<Result<ShowPhotoSizesResponse>>
-
-  fun showPhotoSizes(photoId: String): Call<ShowPhotoSizesResponse>
+  fun showPhotoSizesRx(photoId: String): Single<Result<ShowPhotoSizesResponse>>
 }
