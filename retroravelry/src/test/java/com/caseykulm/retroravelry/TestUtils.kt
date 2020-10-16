08a1d79baa1d577c1c360f732771cb9153a0d162
Casey Kulm <mlukyesac@gmail.com>
2020-10-15 21:02:04 -0600

    Add tests for the coroutine APIs

--- retroravelry/src/test/java/com/caseykulm/retroravelry/TestUtils.kt
+++ retroravelry/src/test/java/com/caseykulm/retroravelry/TestUtils.kt
@@ -1,13 +1,16 @@
 package com.caseykulm.retroravelry
 
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okio.Okio
 import java.io.IOException
 import java.io.InputStream
+import java.util.Date
 
 @Throws(IOException::class)
 fun readFile(stream: InputStream): String {
-    Okio.buffer(Okio.source(stream)).use({ source -> return source.readUtf8() })
+    Okio.buffer(Okio.source(stream)).use { source -> return source.readUtf8() }
 }
 
 fun <T> Class<T>.readResourceFile(filename: String): String {
@@ -19,7 +22,10 @@ fun <T> Class<T>.readResourceFile(filename: String): String {
 @Throws(Exception::class)
 fun <ResponseType> parseJsonResourceFile(filename: String, responseClass: Class<ResponseType>): ResponseType {
     val responseStr = responseClass.readResourceFile(filename)
-    val moshi = Moshi.Builder().build()
+    val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
+        .build()
     val jsonAdapter = moshi.adapter<ResponseType>(responseClass)
     return jsonAdapter.fromJson(responseStr)!!
 }
